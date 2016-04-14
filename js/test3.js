0e1160809477e89b06e7ffa130a302e596da39fe
austinkelleher <austin.kell47@gmail.com>
2016-04-14 13:09:05 -0400

    Change URL

--- js/test3.js
+++ js/test3.js
@@ -1,5 +1,5 @@
 (function() {
     var e = document.createElement('iframe');
-    e.setAttribute("src", "https://www.w3schools.com");
+    e.setAttribute("src", "https://en.wikipedia.org/wiki/W3Schools");
     document.body.appendChild(e);
 })();
